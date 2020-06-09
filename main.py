c0bd02c64a8b1eaba7c78595be9cbb7d79f01a0a
Mon Gomez <nulo76@yahoo.es>
2020-06-09 14:20:02 +0100

    bola rebota

--- main.py
+++ main.py
@@ -6,17 +6,33 @@ YELLOW= (255, 255, 0)
 
 class Ball:
     def __init__(self):
-        self.vx= 5  #veloc. de x e y
-        self.vy= 5
+        self.vx= 2  #veloc. de x e y
+        self.vy= 2
         self.x= 400
         self.y= 300
         self.w= 20  #ancho de la bola
-        self.posX= self.x- self.w//2   # coordenada X para situar la bola en el centro de la pantalla
         self.h= 20   # alto de la bola
-        self.posY= self.y- self.h//2
 
         self.image= pg.Surface((20, 20))
         self.image.fill(YELLOW)
+        
+    @property
+    def posX(self):
+        return  self.x- self.w//2   # coordenada X para situar la bola en el centro de la pantalla
+        
+    @property
+    def posY(self):
+        return self.y- self.h//2
+
+    def move(self, limitSuperX, limitSuperY):
+        if self.x >= limitSuperX or self.x <= 0:
+            self.vx *= -1
+            
+        if self.y >= limitSuperY or self.y <= 0:
+            self.vy *= -1
+
+        self.x += self.vx
+        self.y += self.vy
 
 
 class Game:
@@ -25,8 +41,8 @@ class Game:
         pg.display.set_caption('Pong')
         self.pantalla= pg.display.set_mode((800, 600))
         self.pantalla.fill(BGColor)
-        fondo= pg.image.load('./resources/fondo.jpg')
-        self.pantalla.blit(fondo, (0, 0))
+        self.fondo= pg.image.load('./resources/fondo.jpg')
+        
         
         self.ball= Ball()
 
@@ -40,7 +56,13 @@ class Game:
                 if event.type== pg.QUIT:
                     game_over= True
 
+            
+            self.pantalla.blit(self.fondo, (0, 0))
             self.pantalla.blit(self.ball.image, (self.ball.posX, self.ball.posY))
+
+            self.ball.move(800, 600)
+            
+            
             pg.display.flip()
 
     def quit(self):
