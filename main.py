f7bbd997932f9682703068399728298020527e67
Mon Gomez <nulo76@yahoo.es>
2020-06-09 13:26:35 +0100

    posicionando la bola

--- main.py
+++ main.py
@@ -2,13 +2,35 @@ import pygame as pg
 import sys
 
 BGColor= (0,0,0)
+YELLOW= (255, 255, 0)
+
+class Ball:
+    def __init__(self):
+        self.vx= 5  #veloc. de x e y
+        self.vy= 5
+        self.x= 400
+        self.y= 300
+        self.w= 20  #ancho de la bola
+        self.posX= self.x- self.w//2   # coordenada X para situar la bola en el centro de la pantalla
+        self.h= 20   # alto de la bola
+        self.posY= self.y- self.h//2
+
+        self.image= pg.Surface((20, 20))
+        self.image.fill(YELLOW)
+
 
 class Game:
 
     def __init__(self):
+        pg.display.set_caption('Pong')
         self.pantalla= pg.display.set_mode((800, 600))
         self.pantalla.fill(BGColor)
-        pg.display.set_caption('Pong')
+        fondo= pg.image.load('./resources/fondo.jpg')
+        self.pantalla.blit(fondo, (0, 0))
+        
+        self.ball= Ball()
+
+        
 
     def main_loop(self):
         game_over = False
@@ -18,6 +40,7 @@ class Game:
                 if event.type== pg.QUIT:
                     game_over= True
 
+            self.pantalla.blit(self.ball.image, (self.ball.posX, self.ball.posY))
             pg.display.flip()
 
     def quit(self):
