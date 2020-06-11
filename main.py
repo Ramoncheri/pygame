3f248eec7d1fdd2e25a8795c0f115816503dc95b
Mon Gomez <nulo76@yahoo.es>
2020-06-11 19:40:49 +0100

    primera version completa

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys, random
 BGColor= (0,0,0)
 YELLOW= (255, 255, 0)
 WHITE= (255, 255, 255)
-win_score= 10
+win_score= 3
 
 class Ball:
     def __init__(self):
@@ -15,6 +15,9 @@ class Ball:
 
         self.image= pg.Surface((self.w, self.h))
         self.image.fill(YELLOW)
+        self.ping= pg.mixer.Sound('./resources/sounds/ping.wav')
+        self.lost_point= pg.mixer.Sound('./resources/sounds/lost-point.wav')
+
         
     @property
     def posX(self):
@@ -28,9 +31,11 @@ class Ball:
         if self.x >= limitSuperX or self.x <= 0:
             self.vx = 0
             self.vy = 0
+            self.lost_point.play()
             
         if self.y >= limitSuperY or self.y <= 0:
             self.vy *= -1
+            self.ping.play()
 
         self.x += self.vx
         self.y += self.vy
@@ -44,10 +49,11 @@ class Ball:
             self.vx *= -1
             self.x += self.vx
             self.y += self.vy
+            self.ping.play()
 
     def reset(self):
         self.vx= 2 * random.choice([-2, -1, 1, 2]) #velocidad de la bola en  x e y
-        self.vy= random.choice([-4, -2, 1, 3, 5])
+        self.vy= random.choice([-4, -2, 3, 4])
         self.x= 400
         self.y= 300
 
@@ -92,10 +98,17 @@ class Game:
         self.pantalla= pg.display.set_mode((800, 600))
         self.pantalla.fill(BGColor)
         self.fondo= pg.image.load('./resources/image/fondo.jpg')
-        self.font= pg.font.Font('./resources/fonts/PressStart2P.ttf', 36)
+        self.fontMarcador= pg.font.Font('./resources/fonts/PressStart2P.ttf', 36)
+        self.font= pg.font.Font('./resources/fonts/PressStart2P.ttf', 20)
+        self.fontGrande= pg.font.Font('./resources/fonts/PressStart2P.ttf', 60)
 
-        self.marcadorOne= self.font.render('0', True, WHITE)
-        self.marcadorTwo= self.font.render('0', True, WHITE)
+        self.status= 'partida'
+
+        self.marcadorOne= self.fontMarcador.render('0', True, WHITE)
+        self.marcadorTwo= self.fontMarcador.render('0', True, WHITE)
+
+        self.text_game_over = self.fontGrande.render('GAME OVER', True, YELLOW)
+        self.text_insert_coin= self.font.render('<SPACE> - Inicio partida', True, WHITE)
 
         self.ball= Ball()
 
@@ -106,11 +119,11 @@ class Game:
         self.scoreTwo= 0
 
 
-
     def handleEvent(self):
+        
         for event in pg.event.get():   #EVENTOS
             if event.type== pg.QUIT:
-                return True
+                self.quit()
             '''
             if event.type == KEYDOWN:
                 if event.key== K_w:
@@ -136,16 +149,17 @@ class Game:
         
         return False
 
-    def main_loop(self):
-        game_over = False
+    def bucle_partida(self):
+        game_over= False
+        self.scoreOne = 0
+        self.scoreTwo = 0
+        self.marcadorOne= self.fontMarcador.render('0', True, WHITE)
+        self.marcadorTwo= self.fontMarcador.render('0', True, WHITE)
 
         while not game_over:
 
             game_over= self.handleEvent()
             
-
-
-
             self.ball.move(800, 600)                #movimiento de los objetos
             self.playerOne.raquet_move(800, 600)
             self.playerTwo.raquet_move(800, 600)
@@ -156,11 +170,12 @@ class Game:
             if self.ball.vx == 0 and self.ball.vy == 0:
                 if self.ball.x >= 800:
                     self.scoreOne += 1
-                    self.marcadorOne= self.font.render(str(self.scoreOne), True, WHITE)
+                    self.marcadorOne= self.fontMarcador.render(str(self.scoreOne), True, WHITE)
+     
                 if self.ball.x <= 0:
                     self.scoreTwo +=1
-                    self.marcadorTwo= self.font.render(str(self.scoreTwo), True, WHITE)
-                if self.scoreOne ==10 or self.scoreTwo == 10:
+                    self.marcadorTwo= self.fontMarcador.render(str(self.scoreTwo), True, WHITE)
+                if self.scoreOne == win_score or self.scoreTwo == win_score:
                     game_over= True
 
                 
@@ -172,7 +187,40 @@ class Game:
             self.pantalla.blit(self.playerTwo.image, (self.playerTwo.posX, self.playerTwo.posY))
             self.pantalla.blit(self.marcadorOne, (340, 10))
             self.pantalla.blit(self.marcadorTwo, (430, 10))
+
             pg.display.flip()
+
+        self.status = 'Inicio'
+
+    def bucle_inicio(self):
+        inicio_partida = False
+        while not inicio_partida:
+            for event in pg.event.get():   #EVENTOS
+                if event.type== QUIT:
+                    self.quit()
+                
+                if event.type == KEYDOWN:
+                    if event.key== K_SPACE:
+                        inicio_partida= True
+
+            self.pantalla.fill((0, 0, 255))
+            self.pantalla.blit(self.text_game_over,(100, 100))
+            self.pantalla.blit(self.text_insert_coin,(100, 200))
+
+            pg.display.flip()
+
+        self.status = 'partida'
+
+    
+    def main_loop(self):
+
+        while True:
+
+            if self.status== 'partida':
+                self.bucle_partida()
+            else:
+                self.bucle_inicio()
+
 
     def quit(self):
         pg.quit()
